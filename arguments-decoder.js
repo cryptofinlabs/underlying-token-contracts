99df2e80eefa763446fb8f86a865e3a60e228dc2
Daniel Que <dmdque@gmail.com>
2018-02-14 20:01:45 -0800

    Update scripts

--- arguments-decoder.js
+++ arguments-decoder.js
@@ -1,14 +1,18 @@
 let abi = require("ethereumjs-abi");
 
-let SIZE = 2;
-for (let token of ["eos", "qtum"]) {
-  let args = require(`./${token}/args.json`);
-  let decodedBuffer = abi.rawDecode(args.types, new Buffer(args.rawdata, 'hex'));
-  let decodedString = decodedBuffer.toString('hex');
-  let decodedValues = decodedString.split(',');
-  console.log(token);
-  for (let i = 0; i < args.argument_names.length; i++) {
-    console.log(`${args.argument_names[i]}: ${decodedValues[i]}`);
+let tokens = ["QASH", "0x", "populous", "Loopring", "vechain", "Aeternity", "binance-coin", "IOSToken", "powerledger", "qtum", "Status", "augur", "nebulas", "Walton", "OmiseGo", "tron", "Dragon", "Kyber", "aelf", "golem", "BytomToken", "digixdao", "eos", "basic-attention-token", "Zilliqa"];
+for (let token of tokens) {
+  try {
+    let args = require(`./${token}/args.json`);
+    let decodedBuffer = abi.rawDecode(args.types, new Buffer(args.rawdata, 'hex'));
+    let decodedString = decodedBuffer.toString('hex');
+    let decodedValues = decodedString.split(',');
+    console.log(token);
+    for (let i = 0; i < args.argument_names.length; i++) {
+      console.log(`${args.argument_names[i]}: ${decodedValues[i]}`);
+    }
+  } catch (e) {
+    console.log(`${token} skip`);
   }
 }
 
--- arguments-encoder.js
+++ arguments-encoder.js
@@ -1,12 +1,16 @@
 let abi = require("ethereumjs-abi");
 
-for (let token of ["eos", "tron", "qtum"]) {
-  console.log(token);
-  let args = require(`./${token}/args.json`);
-  let encoded = abi.rawEncode(args.types, args.values);
-  let encodedString = encoded.toString("hex");
-  console.log(encodedString);
-  console.log(encodedString === args.rawdata ? "rawdata matches" : "rawdata doesn't match");
+let tokens = ["QASH", "0x", "populous", "Loopring", "vechain", "Aeternity", "binance-coin", "IOSToken", "powerledger", "qtum", "Status", "augur", "nebulas", "Walton", "OmiseGo", "tron", "Dragon", "Kyber", "aelf", "golem", "BytomToken", "digixdao", "eos", "basic-attention-token", "Zilliqa"];
+for (let token of tokens) {
+  try {
+    console.log(token);
+    let args = require(`./${token}/args.json`);
+    let encoded = abi.rawEncode(args.types, args.values);
+    let encodedString = encoded.toString("hex");
+    console.log(encodedString);
+    console.log(encodedString === args.rawdata ? "rawdata matches" : "rawdata doesn't match");
+  } catch (e) {
+  }
 }
 
 //module.exports()
